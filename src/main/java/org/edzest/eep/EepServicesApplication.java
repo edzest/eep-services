905fa3690049ef693e7911bb64a6963fd687b11e
Piyush Ranjan <piyushranjan95@gmail.com>
2022-02-05 13:49:30 +0530

    Initial Commit

---
+++ src/main/java/org/edzest/eep/EepServicesApplication.java
@@ -0,0 +1,13 @@
+package org.edzest.eep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EepServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EepServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/edzest/eep/EepServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edzest.eep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EepServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
