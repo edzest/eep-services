091902dcba7fcab0dea51724d423af933e2d2956
Piyush Ranjan <piyushranjan95@gmail.com>
2022-02-27 09:54:44 +0530

    Add cors origins

--- src/main/java/org/edzest/eep/controllers/TestController.java
+++ src/main/java/org/edzest/eep/controllers/TestController.java
@@ -3,6 +3,7 @@ package org.edzest.eep.controllers;
 import org.edzest.eep.entities.TestInfo;
 import org.edzest.eep.services.TestInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tests")
+@CrossOrigin(origins = {"http://localhost:4200/", "https://eep-dashboard.web.app"})
 public class TestController {
 
     @Autowired
